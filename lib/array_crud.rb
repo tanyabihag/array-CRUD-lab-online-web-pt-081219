6c88a893ce79721ec066cdd1e86993879a0502a3
Tanya Bihag <tbihag@gmail.com>
2019-08-15 04:45:54 +0000

    Automatically backed up by Learn

--- lib/array_crud.rb
+++ lib/array_crud.rb
@@ -1,12 +1,13 @@
-def create_an_empty_array
+def create_an_empty_array []
   
 end
 
-def create_an_array
+def create_an_array [1, 2, 3]
   
 end
 
 def add_element_to_end_of_array(array, element)
+  add_element_to_end_of_array << "Hi"
   
 end
 
