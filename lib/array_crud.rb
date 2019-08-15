c1a80d06e78a2ceedbc8874fb441b6895a762bba
Tanya Bihag <tbihag@gmail.com>
2019-08-15 04:48:52 +0000

    Automatically backed up by Learn

--- lib/array_crud.rb
+++ lib/array_crud.rb
@@ -17,21 +17,26 @@ def add_element_to_start_of_array(array, element)
 end
 
 def remove_element_from_end_of_array(array)
+  remove_element_from_end_of_array.pop
   
 end
 
 def remove_element_from_start_of_array(array)
+  remove_element_from_start_of_array.shift 
   
 end
 
 def retrieve_element_from_index(array, index_number)
+  retrieve_element_from_index[0]
   
 end
 
 def retrieve_first_element_from_array(array)
+  retrieve_first_element_from_array.first
   
 end
 
 def retrieve_last_element_from_array(array)
+  retrieve_last_element_from_array.last
   
 end
